0a352663342a984824cc6e5962c90ef252c3b3ac
amar3344 <amarkumarreddynew@gmail.com>
2022-09-30 16:41:42 +0000

    done

---
+++ src/components/AppointmentItem/index.js
@@ -0,0 +1,26 @@
+import {format} from 'date-fns'
+import './index.css'
+
+const AppointmentItem = props => {
+  const {commentDetails} = props
+  const {titleInput, dateInput, isStarred, id} = commentDetails
+  const starredImage = isStarred
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  return (
+    <li className="appointment-list">
+      <div className="title-starred-container">
+        <h1 className="appointment-title">{titleInput}</h1>
+        <button type="button" className="star-image-button">
+          {starredImage}
+        </button>
+      </div>
+      <p className="date-format">
+        Date:{format(new Date({dateInput}), 'dd MMMM yyyy, EEEE')}
+      </p>
+    </li>
+  )
+}
+
+export default AppointmentItem
---
+++ src/components/Appointments/index.js
@@ -0,0 +1,96 @@
+import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+
+import {format} from 'date-fns'
+
+import AppointmentItem from '../AppointmentItem'
+import './index.css'
+
+class Appointments extends Component {
+  state = {appointmentsList: [], titleInput: '', dateInput: ''}
+
+  getTitle = event => {
+    this.setState({titleInput: event.target.value})
+  }
+
+  getDate = event => {
+    this.setState({dateInput: event.target.value})
+  }
+
+  getFormRequest = event => {
+    event.preventDefault()
+
+    const {titleInput, dateInput} = this.state
+
+    const addAppointment = {
+      id: uuidv4(),
+      titleInput,
+      dateInput,
+      isStarred: false,
+    }
+
+    this.setState(prevState => ({
+      appointmentsList: [...prevState.appointmentsList, addAppointment],
+    }))
+  }
+
+  getAppointmentsList = () => {
+    const {appointmentsList} = this.state
+
+    return appointmentsList.map(eachAppointment => (
+      <AppointmentItem
+        key={eachAppointment.id}
+        commentDetails={eachAppointment}
+      />
+    ))
+  }
+
+  render() {
+    return (
+      <div className="main-container">
+        <div className="bg-container">
+          <div className="container">
+            <h1 className="heading">Add Appointment</h1>
+            <form className="form" onSubmit={this.getFormRequest}>
+              <label className="title-text" htmlFor="title">
+                TITLE
+              </label>
+              <input
+                className="input-title"
+                type="text"
+                id="title"
+                placeholder="Title"
+                onChange={this.getTitle}
+              />
+              <label className="date-text" htmlFor="date">
+                DATE
+              </label>
+              <input
+                className="date-input"
+                type="date"
+                placeholder="dd/mm/yyyy"
+                id="date"
+                onChange={this.getDate}
+              />
+              <button className="button" type="submit">
+                Add
+              </button>
+            </form>
+            <hr className="horizontal-line" />
+            <div className="appointments-container">
+              <h1 className="heading">Appointments</h1>
+              <button type="button" className="button-starred">
+                Starred
+              </button>
+            </div>
+            <ul className="appointment-list-container">
+              {this.getAppointmentsList()}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Appointments
