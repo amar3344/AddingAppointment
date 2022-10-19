a70b2aad5e33a16dd7c9760f1f9e359512ae9b70
amar3344 <amarkumarreddynew@gmail.com>
2022-10-19 07:21:15 +0000

    4th commit

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -5,7 +5,12 @@ import AppointmentItem from '../AppointmentItem/index'
 import './index.css'
 
 class Appointments extends Component {
-  state = {appointmentsList: [], titleInput: '', dateInput: ''}
+  state = {
+    appointmentsList: [],
+    titleInput: '',
+    dateInput: '',
+    isFilterActive: false,
+  }
 
   getTitle = event => {
     this.setState({titleInput: event.target.value})
@@ -35,47 +40,53 @@ class Appointments extends Component {
   }
 
   getAppointmentsList = () => {
-    const {appointmentsList} = this.state
+    const {appointmentsList, isFilterActive} = this.state
 
-    return appointmentsList.map(eachAppointment => (
-      <AppointmentItem
-        key={eachAppointment.id}
-        appointmentDetails={eachAppointment}
-      />
-    ))
+    if (isFilterActive) {
+      return appointmentsList.filter(
+        eachAppointment => eachAppointment.isStarred === true,
+      )
+    }
+    return appointmentsList
   }
 
   render() {
+    const {titleInput, dateInput} = this.state
+    const filteredAppointmentList = this.getAppointmentsList()
     return (
       <div className="main-container">
         <div className="bg-container">
           <div className="container">
-            <h1 className="heading">Add Appointment</h1>
-            <form className="form" onSubmit={this.getFormRequest}>
-              <label className="title-text" htmlFor="title">
-                TITLE
-              </label>
-              <input
-                className="input-title"
-                type="text"
-                id="title"
-                placeholder="Title"
-                onChange={this.getTitle}
-              />
-              <label className="date-text" htmlFor="date">
-                DATE
-              </label>
-              <input
-                className="date-input"
-                type="date"
-                placeholder="dd/mm/yyyy"
-                id="date"
-                onChange={this.getDate}
-              />
-              <button className="button" type="submit">
-                Add
-              </button>
-            </form>
+            <div>
+              <h1 className="heading">Add Appointment</h1>
+              <form className="form" onSubmit={this.getFormRequest}>
+                <label className="title-text" htmlFor="title">
+                  TITLE
+                </label>
+                <input
+                  className="input-title"
+                  type="text"
+                  id="title"
+                  placeholder="Title"
+                  value={titleInput}
+                  onChange={this.getTitle}
+                />
+                <label className="date-text" htmlFor="date">
+                  DATE
+                </label>
+                <input
+                  className="date-input"
+                  type="date"
+                  placeholder="dd/mm/yyyy"
+                  id="date"
+                  value={dateInput}
+                  onChange={this.getDate}
+                />
+                <button className="button" type="submit">
+                  Add
+                </button>
+              </form>
+            </div>
             <hr className="horizontal-line" />
             <div className="appointments-container">
               <h1 className="heading">Appointments</h1>
@@ -83,9 +94,16 @@ class Appointments extends Component {
                 Starred
               </button>
             </div>
-            <ul className="appointment-list-container">
-              {this.getAppointmentsList()}
-            </ul>
+            <div>
+              <ul className="appointment-list-container">
+                {filteredAppointmentList.map(eachAppointment => (
+                  <AppointmentItem
+                    key={eachAppointment.id}
+                    appointmentDetails={eachAppointment}
+                  />
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
